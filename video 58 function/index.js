a68e9ba789d13e1197890c87e84318e549e6540a
HardikKapil1 <kapilhardik11@gmail.com>
2025-09-24 09:25:48 +0530

    Add examples for functions, strings, arrays, and DOM
    Introduced new example files covering JavaScript functions, string manipulation, array basics, and DOM operations. Added HTML and JS files for each topic, including function demonstrations, a random operation calculator, random business name generator, string indexing, array usage, and basic DOM manipulation.

--- video 58 function/index.js
+++ video 58 function/index.js
@@ -1,4 +1,27 @@
+function nice(name) {
+console.log("Hey " +name+ " you are nice ")    
+console.log("Hey " +name+ " you are nice and good too ")    
+console.log("Hey " +name+ " you are good ")    
+}
+nice("Hap")
+nice("shivam")
+
+function sum(a,b) 
+{
+console.log(a+b)
+    
+}
+sum(3,515165)
+
+console.log("Hey Hardik you are nice")
+console.log("Hey Hardik you are good")
 console.log("Hey Hardik you are nice")
 console.log("Hey Hardik you are nice")
-console.log("Hey Hardik you are nice")
-console.log("Hey Hardik you are nice")
+
+function sum(a,b,c=4) {
+    return a+b+c
+    
+}
+result1=sum(2,3)
+result2=sum(2,53)
+console.log("sum is ", result2,result1)
---
+++ video 60 strings/index.js
@@ -0,0 +1,9 @@
+console.log("this is dtrings tutorial")
+let a = "Hardik"
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
